055660249e04574b0d6eebd2f12f61db97ff18a1
찬기 <limchangi@naver.com>
2020-09-12 21:33:00 +0900

    백엔드 base 정리

--- team3_mini/Backend/app.js
+++ team3_mini/Backend/app.js
@@ -1,25 +1,28 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const app = express();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
+//Routes
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
+
+////Middleware
 //cors 
 const cors = require('cors');
 app.use(cors());
 
 //body Parser 
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -27,23 +30,27 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+//Handling error
+app.use((req,res,next)=>{
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use((error,req,res,next)=>{
+  res.status(error.status || 500);
+  res.json({
+      error:{
+         message: error.message
+      }
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  });
+
+
 });
 
 module.exports = app;
