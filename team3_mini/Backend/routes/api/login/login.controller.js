f4be45e8ff1c2f1e8b028fdcc3add1503ae65dbe
jjmmll0727 <jjmmll0727@gmail.com>
2020-09-14 15:04:22 +0900

    ...

---
+++ team3_mini/Backend/routes/api/login/login.controller.js
@@ -0,0 +1,11 @@
+// 로그인에 대한 로직구현
+
+//get & post
+
+const express = require('express');
+const router = express.Router();
+const crypto = require("crypto");
+
+exports.get = (req, res) => {
+    res.send('here is for sign_up');
+}
