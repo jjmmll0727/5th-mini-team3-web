b65d58c0cfb7e921773690d4dcfd6d38bb66494d
찬기 <limchangi@naver.com>
2020-09-15 14:15:13 +0900

    회원가입 오류 확인 추가

--- team3_mini/Backend/routes/api/sign_up/sign_up.controller.js
+++ team3_mini/Backend/routes/api/sign_up/sign_up.controller.js
@@ -8,39 +8,48 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 
 
-exports.post = (req,res,next)=>{
-    const newUser = new User({
+exports.post = (req, res, next) => {
+    User.findOne({ userId: req.body.userId }).exec().then(user => {
+        if (user) {
+            return res.status(409).json({
+                message: "이미 존재하는 ID 입니다"
 
-        userId : req.body.userId,
-        password : req.body.password,
-        birth : req.body.birth,
-        email : req.body.email,
-        phone : req.body.phone,
-        homephone : req.body.homephone
-
-    });
-
-    bcrypt.genSalt(10, (err, salt) => {
-
-        bcrypt.hash(newUser.password, salt, (err, hash) => {
-            newUser.password = hash;
-            newUser.save().then(result=>{
-                console.log(result);
-            }).catch((err)=>{
-                console.log(err);
             });
-        });
+        } else {
+            const newUser = new User({
 
-    });
-    res.status(201).json({
-        message: "POST 요청을 통한 사용자데이터 저장",
-        savedUser : newUser
-    });
+                userId: req.body.userId,
+                password: req.body.password,
+                birth: req.body.birth,
+                email: req.body.email,
+                phone: req.body.phone,
+                homephone: req.body.homephone
 
+            });
 
+            bcrypt.genSalt(10, (err, salt) => {
+
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    newUser.password = hash;
+                    newUser.save().then(result => {
+                        console.log(result);
+                        res.status(201).json({
+                            message: "POST 요청을 통한 사용자데이터 저장",
+                            savedUser: newUser
+                        });
+                    }).catch((err) => {
+                        console.log(err);
+                        res.status(500).json({
+                            error: err
+
+                        });
+                    });
+                });
+            })
+        };
+    })
 
 }
-
 
 
 exports.get = (req, res) => {
