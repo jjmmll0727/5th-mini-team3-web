d1c5d1513d80a31594b847a1ebfbe18b17c630d9
찬기 <limchangi@naver.com>
2020-09-16 12:25:23 +0900

    jwt체크 route 추가

--- team3_mini/Backend/routes/api/login/index.js
+++ team3_mini/Backend/routes/api/login/index.js
@@ -1,9 +1,15 @@
 const router = require('express').Router();
 const { render } = require('pug');
 const controller = require('./login.controller');
+const {verifyToken} = require('../middleware/jwt-verify');
+// 이 부분은 나중에 사용자가 로그인해야 접근할수 있는 라우팅에 
+// exports.OOO(verifytoken,(req, res) => 형태로 사용하면됩니다!
+
 
 router.get('/get', controller.get);
 
 router.post('/create', controller.login);
+
+router.get('/testjwt',verifyToken, controller.testjwt);
 
 module.exports = router
--- team3_mini/Backend/routes/api/models/User.js
+++ team3_mini/Backend/routes/api/models/User.js
@@ -15,7 +15,7 @@ const UserSchema = new Schema({
         type: String,
         required: true,
         trim : true
-
+        
     },
     birth : {
         type: String,
