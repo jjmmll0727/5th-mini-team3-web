30e4c4a95d75b5b4db497b69fb0a1473440a2d3e
찬기 <limchangi@naver.com>
2020-09-25 17:36:52 +0900

    사이트 delete 추가

--- team3_mini/Frontend/.next/server/pages/index.js
+++ team3_mini/Frontend/.next/server/pages/index.js
@@ -108,7 +108,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _emotion_styled__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_emotion_styled__WEBPACK_IMPORTED_MODULE_1__);
 /* harmony import */ var prop_types__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! prop-types */ "prop-types");
 /* harmony import */ var prop_types__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(prop_types__WEBPACK_IMPORTED_MODULE_2__);
-var _jsxFileName = "C:\\Users\\jjmml\\Documents\\GitHub\\5th-mini-team3-web\\team3_mini\\Frontend\\components\\Card.js";
+var _jsxFileName = "C:\\Users\\user\\Desktop\\NodeJS\\5th-mini-team3-web\\team3_mini\\Frontend\\components\\Card.js";
 var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
 
 
@@ -238,7 +238,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _emotion_styled__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @emotion/styled */ "@emotion/styled");
 /* harmony import */ var _emotion_styled__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(_emotion_styled__WEBPACK_IMPORTED_MODULE_4__);
 /* harmony import */ var _components_Card__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../components/Card */ "./components/Card.js");
-var _jsxFileName = "C:\\Users\\jjmml\\Documents\\GitHub\\5th-mini-team3-web\\team3_mini\\Frontend\\pages\\index.js";
+var _jsxFileName = "C:\\Users\\user\\Desktop\\NodeJS\\5th-mini-team3-web\\team3_mini\\Frontend\\pages\\index.js";
 var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
 
 
--- team3_mini/Frontend/.next/server/pages/login.js
+++ team3_mini/Frontend/.next/server/pages/login.js
@@ -104,7 +104,7 @@ module.exports =
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "react");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
-var _jsxFileName = "C:\\Users\\jjmml\\Documents\\GitHub\\5th-mini-team3-web\\team3_mini\\Frontend\\pages\\login.js";
+var _jsxFileName = "C:\\Users\\user\\Desktop\\NodeJS\\5th-mini-team3-web\\team3_mini\\Frontend\\pages\\login.js";
 var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
 
 
